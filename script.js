3f1156b5fc9939006e9ccdd733e9c418eaf1fe24
takari58 <e082517115@niigata-pc.ac.jp>
2026-05-18 10:29:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const btn = document.getElementById("btn");
 const message = document.getElementById("message");
 
 btn.addEventListener("click", () => {
-    message.textContent = "ボタンがクリックされました！";
+    message.textContent = "クリックされました！";
 });
